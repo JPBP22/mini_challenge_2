d1ecea83bbff0978023e45f021e33b4489319149
JPBP22 <79642745+JPBP22@users.noreply.github.com>
2023-10-08 13:45:33 +0200

    Update main.dart
    button works

--- task_app/lib/main.dart
+++ task_app/lib/main.dart
@@ -8,20 +8,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Pending Tasks')),
-        body: ListView.builder(
-          itemCount: 10, // placeholder, replace with dynamic list later
-          itemBuilder: (context, index) {
-            return ListTile(title: Text('Task $index'));
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Will implement dialog or bottom sheet
-          },
-          child: Icon(Icons.add),
-        ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  Future<void> _displayAddTaskDialog(BuildContext context) async {
+    String taskTitle, taskDescription;
+    DateTime dueDate;
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Add New Task'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  decoration: InputDecoration(labelText: 'Task Title'),
+                  onChanged: (value) {
+                    taskTitle = value;
+                  },
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Task Description'),
+                  onChanged: (value) {
+                    taskDescription = value;
+                  },
+                ),
+              ],
+            ),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Add'),
+              ),
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ToDo App'),
+      ),
+      body: Center(
+        child: Text('List of tasks will appear here'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _displayAddTaskDialog(context);
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
